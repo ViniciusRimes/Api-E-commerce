181d4f3942a09be52c7b1c1b655e72b341d91470
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2023-11-03 14:56:12 -0300

    security

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,6 @@
 const {Sequelize} = require('sequelize')
-const db = new Sequelize('e_commerce_api', 'root', '16042003', {
+require('dotenv').config()
+const db = new Sequelize('e_commerce_api', process.env.USER, process.env.PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
 })
